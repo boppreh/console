0dc6121e2dc758cf0f6774d403172d7b35f99d6e
BoppreH <boppreh@gmail.com>
2013-10-03 01:10:52 -0300

    Add basic code

---
+++ console.py
@@ -0,0 +1,54 @@
+import os, sys
+try:
+    from msvcrt import getch
+    special_keys = {
+        'H': 'up', 'P': 'down',
+        'M': 'right', 'K': 'left',
+    }
+
+    def get_key():
+        char = getch()
+        # Arrow keys are returned as two bytes, starting with 224.
+        if ord(char) == 224:
+            return special_keys[getch()]
+        else:
+            return char
+
+    def display(text):
+        os.system('cls')
+        sys.stdout.write(text)
+
+except ImportError:
+    import curses, atexit
+    window = curses.initscr()
+    window.keypad(True)
+    curses.noecho()
+    curses.cbreak()
+
+    # Without this the program works fine, but the terminal remains borked
+    # after exit. If this happens to you, run `reset` to restore (just type and
+    # press enter, even if you don't see the text).
+    atexit.register(curses.endwin)
+    atexit.register(curses.nocbreak)
+    atexit.register(curses.echo)
+
+    special_keys = {
+        259: 'forward', 258: 'backward',
+        261: 'right', 260: 'left',
+    }
+
+    # Note: window.getch() returns int, not str.
+    def get_key():
+        keycode = window.getch()
+        if keycode > 256:
+            return special_keys[keycode]
+        else:
+            return chr(keycode)
+
+    def display(text):
+        window.addstr(0, 0, text)
+        window.clrtobot()
+        window.refresh()
+
+if __name__ == '__main__':
+    print get_key()
